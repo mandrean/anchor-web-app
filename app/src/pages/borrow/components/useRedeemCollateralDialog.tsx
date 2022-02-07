23fa03cf775bc20bf78ca9284ef950fff4351ae1
simke9445 <simke9445@gmail.com>
2022-02-07 03:05:06 +0100

    Fix CI lint issues.

--- app/src/pages/borrow/components/useRedeemCollateralDialog.tsx
+++ app/src/pages/borrow/components/useRedeemCollateralDialog.tsx
@@ -1,9 +1,9 @@
 import { BorrowBorrower } from '@anchor-protocol/app-fns';
 import {
+  BorrowMarketWithDisplay,
   useBorrowRedeemCollateralForm,
   useBorrowRedeemCollateralTx,
 } from '@anchor-protocol/app-provider';
-import { BorrowMarketWithDisplay } from '@anchor-protocol/app-provider';
 import {
   formatBAsset,
   formatBAssetInput,
